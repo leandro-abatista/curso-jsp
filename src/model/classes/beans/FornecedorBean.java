f05840da289fc378de88e168c26f28940fb99282
Leandro Amorim <leandroamorimpb@gmail.com>
2021-03-12 11:23:30 -0300

    Criando o Bean Fornecedor

---
+++ src/model/classes/beans/FornecedorBean.java
@@ -0,0 +1,74 @@
+package model.classes.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class FornecedorBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long codigo;
+	private String razaoSocial;
+	private String nomeFantasia;
+	private String cnpj;
+	private String inscricaoEstadual;
+	private String inscricaoMunicipal;
+	private Date dataCadastro;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscricaoEstadual() {
+		return inscricaoEstadual;
+	}
+
+	public void setInscricaoEstadual(String inscricaoEstadual) {
+		this.inscricaoEstadual = inscricaoEstadual;
+	}
+
+	public String getInscricaoMunicipal() {
+		return inscricaoMunicipal;
+	}
+
+	public void setInscricaoMunicipal(String inscricaoMunicipal) {
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+}
