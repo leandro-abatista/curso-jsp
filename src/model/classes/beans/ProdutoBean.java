d6aa96429e889f5c8c1e416e6413726d42d44d7a
Leandro Amorim <leandroamorimpb@gmail.com>
2021-03-13 10:33:31 -0300

    Criando o bean produto

---
+++ src/model/classes/beans/ProdutoBean.java
@@ -0,0 +1,56 @@
+package model.classes.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long codigo;
+	private String nome;
+	private Double valor;
+	private Integer quantidade;
+	private Date dataCadastro;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+}
